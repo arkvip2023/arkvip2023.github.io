ea18878d3c2f3ea8baa91f82ef393796a11d1a78
arkvip <adlink29@gmail.com>
2025-03-30 16:23:33 +0800

    Site updated: 2025-03-30 16:23:32

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/30/hello-world/"];function toRandomPost(){
+var posts=["2025/03/30/hello-world/","2025/03/30/ggyys1/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
